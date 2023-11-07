b2681aaeb6baf1b1ea5ec4ec53e8750b4ff96862
dacruzmathis <mathisdaccruz@gmail.com>
2023-11-07 19:19:04 +0100

    end of practice

---
+++ use_blob_auth.py
@@ -0,0 +1,31 @@
+import os
+import uuid
+
+from azure.identity import DefaultAzureCredential
+
+# Import the client object from the SDK library
+from azure.storage.blob import BlobClient
+
+from dotenv import load_dotenv
+
+# Chargez les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+credential = DefaultAzureCredential()
+
+# Retrieve the storage blob service URL, which is of the form
+# https://<your-storage-account-name>.blob.core.windows.net/
+storage_url = os.getenv("AZURE_STORAGE_BLOB_URL")
+
+# Create the client object using the storage URL and the credential
+blob_client = BlobClient(
+    storage_url,
+    container_name="blob-container-01",
+    blob_name=f"sample-blob-{str(uuid.uuid4())[0:5]}.txt",
+    credential=credential,
+)
+
+# Open a local file and upload its contents to Blob Storage
+with open("./sample-source.txt", "rb") as data:
+    blob_client.upload_blob(data)
+    print(f"Uploaded sample-source.txt to {blob_client.url}")
