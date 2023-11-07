354f498c94aaf797aa058349e8b3f08adf285511
dacruzmathis <mathisdaccruz@gmail.com>
2023-11-07 12:00:08 +0100

    let's practise

--- provision_rg.py
+++ provision_rg.py
@@ -4,11 +4,16 @@ import os
 from azure.identity import AzureCliCredential
 from azure.mgmt.resource import ResourceManagementClient
 
+from dotenv import load_dotenv
+
+# Chargez les variables d'environnement depuis le fichier .env
+load_dotenv()
+
 # Acquire a credential object using CLI-based authentication.
 credential = AzureCliCredential()
 
 # Retrieve subscription ID from environment variable.
-subscription_id = '794eeba6-7c30-4735-a23f-20dc0d231103'
+subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
 
 # Obtain the management object for resources.
 resource_client = ResourceManagementClient(credential, subscription_id)
